d4fbc4fcb5639f11bf597792ba484cea7273fbb6
jenniferkirwin <lamontagneartist@gmail.com>
2020-05-31 17:06:58 -0400

    Updated files to match original work

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { SavedPageComponent } from './pages/saved-page/saved-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'saved', component: SavedPageComponent },
+  { path: '', component: SearchPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,45 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardItemComponent } from './components/card-item/card-item.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { SavedPageComponent } from './pages/saved-page/saved-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+
+// Materialize Components
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardItemComponent,
+    NavigationComponent,
+    SearchbarComponent,
+    SavedPageComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // Start Materialize Components
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
