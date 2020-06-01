508982ddb21c5b78d2407c40a2d76692ca1ef429
jenniferkirwin <lamontagneartist@gmail.com>
2020-06-01 11:05:31 -0400

    Created basic search bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,6 +40,7 @@ import { MatTableModule } from '@angular/material/table';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     // Start Materialize Components
     MatMenuModule,
     MatButtonModule,
--- client/src/app/components/searchbar/searchbar.component.ts
+++ client/src/app/components/searchbar/searchbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { SearchQuery } from '../../searchquery';
 
 @Component({
   selector: 'app-searchbar',
@@ -7,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchbarComponent implements OnInit {
 
+  // searchQuery = new SearchQuery('Search a Book!');
 
-  constructor() { }
+  searchQuery: string;
+
+  submitted = false;
+
+  onSubmit() { 
+    this.submitted = true; 
+    console.log(this.searchQuery);
+    this.searchQuery = '';
+  }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.searchQuery); }
 
   ngOnInit(): void {
   }
